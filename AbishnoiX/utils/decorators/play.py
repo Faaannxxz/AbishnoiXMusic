a32faac78cc52f87853403fb85415ffdd842b36f
A-Bishnoi <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-12 23:25:04 +0530

    Add files via upload

--- AbishnoiX/utils/decorators/play.py
+++ AbishnoiX/utils/decorators/play.py
@@ -2,15 +2,15 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE, adminlist
 from strings import get_string
-from AnonX import YouTube, app
-from AnonX.misc import SUDOERS
-from AnonX.utils.database import (get_cmode, get_lang,
+from AbishnoiX import YouTube, app
+from AbishnoiX.misc import SUDOERS
+from AbishnoiX.utils.database import (get_cmode, get_lang,
                                        get_playmode, get_playtype,
                                        is_active_chat,
                                        is_commanddelete_on,
                                        is_served_private_chat)
-from AnonX.utils.database.memorydatabase import is_maintenance
-from AnonX.utils.inline.playlist import botplaylist_markup
+from AbishnoiX.utils.database.memorydatabase import is_maintenance
+from AbishnoiX.utils.inline.playlist import botplaylist_markup
 
 
 def PlayWrapper(command):
